f3f01154760df8f12d0d1fba208eb5aa3ee1f392
kyle coulter <152663440+coulterkyle@users.noreply.github.com>
2024-02-27 10:11:34 -0500

    removed commented lines

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth')
-// const routes = require('./routes');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -12,7 +11,6 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
-  // introspection: true,
   typeDefs,
   resolvers,
 });
