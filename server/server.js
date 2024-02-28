e69b3615d9a748a9632df7a0048b26fc8344976a
kyle coulter <152663440+coulterkyle@users.noreply.github.com>
2024-02-28 09:50:02 -0500

    server.js app.get out of if

--- server/server.js
+++ server/server.js
@@ -31,10 +31,10 @@ const startApolloServer = async () => {
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
   }
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 
 
 
